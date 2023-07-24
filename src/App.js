448f9898e8f4aae0ccb089c17319e4c902365c47
Matthew McAlarney <memcalarney@wpi.edu>
2023-07-23 20:47:25 -0400

    Added bar chart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-function App() {
-  return (
-    <div>
-      Hello man.
-    </div>
-  );
+import Spirograph from './component/Spirograph'
+
+//https://d3js.org/d3.v5.min.js
+
+function App()
+{
+  return (<Spirograph />);
 }
 
 export default App;
+
+
+
+
+
