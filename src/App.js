5e2bfdcee21495c8a5acfef4692e2bee4a8440a9
Matthew McAlarney <67719286+MacCode7110@users.noreply.github.com>
2023-08-02 22:30:47 -0400

    Added viewbox for bar chart

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App()
   return (
     <div>
       <Title />
-      <BarChart data={[25, 5, 30, 65, 15, 35]}
-    width={700}
+      <BarChart data={[12, 5, 6, 6, 9, 10]}
+    width={1200}
     height={300}/>
     </div>
     );
--- src/component/BarChart.js
+++ src/component/BarChart.js
@@ -8,46 +8,40 @@ class BarChart extends Component {
     return this.drawBarChart();
   }
 
-  //300
-
   drawBarChart() {
-    const data = this.props.data;
     
-    //Set a variable with different properties
-    let dimensions = {
-      width: this.props.width,
-      height: this.props.height,
-      margins: {
-        top: 15,
-        right: 30,
-        bottom: 0,
-        left: 30,
-      }
-    }
+  //Declare the chart dimensions and margins.
+  const data = this.props.data;
+  const width = this.props.width;
+  const height = this.props.height;
+  const marginTop = 30;
+  const marginRight = 0;
+  const marginBottom = 30;
+  const marginLeft = 40;
 
-    //Create svg
+  //Create the SVG container.
+  const svg = d3.select("#root").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("viewBox", [0, 0, width, height]);
 
-    const svg = d3.select("#root").append("svg").attr("width", dimensions.width).attr("height", dimensions.height).append("g")
-        .style("transform", "translate(" + dimensions.margins.left + "," + dimensions.margins.right + ")");
-
-    //Draw the chart using the svg container:
-
-    svg.selectAll("rect")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("x", (d, i) => i * 70)
-    .attr("y", (d, i) => dimensions.height - 10 * d)
-    .attr("width", dimensions.width)
-    .attr("height", (d, i) => d * 10)
-    .attr("fill", "purple");
+  svg.selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => i * 70)
+      .attr("y", (d, i) => height - 10 * d)
+      .attr("width", 65)
+      .attr("height", (d, i) => d * 10)
+      .attr("fill", "purple");
   }
 
   render() {
+
     return (
       <div>
         <section className = "section is-medium">
-          <div className = "" id={"barChart" + this.props.id}></div>
+          <div id={"barChart" + this.props.id}></div>
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"></link>
         </section>
       </div>  
